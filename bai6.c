c40fe3163840e2f897f9aa74659293a327fa2a35
hoagtutrugvan3010 <thieutiendung3010@gmail.com>
2024-11-21 11:57:29 +0700

    bai6.c

--- bai6.c
+++ bai6.c
@@ -6,10 +6,10 @@
 		int radius = 5;
 		//tinh chuvi va dien tich hinh tron
 		float perimeter = PI * radius * 2;
-		printf(" chu vi hinh tron la %d\n",perimeter);
+		printf(" chu vi hinh tron la %f\n",perimeter);
 		
 		float acreage = PI * radius * radius;
-		printf(" dien tich hinh tron la %d\n",acreage);
+		printf(" dien tich hinh tron la %f\n",acreage);
 		
 		return 0;
 	}
